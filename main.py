62a14e9d5040ed98206fa5cb53ef82d515732655
Samuel Medeiros <steixeiramedeiros@gmail.com>
2026-08-15 01:31:02 -0300

    Add environment variable support for database credentials and update requirements

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+import os
 import firebirdsql
 import psycopg2
+from dotenv import load_dotenv
 
 from database_migrator import DatabaseMigrator
 
+load_dotenv()
+
 if __name__ == '__main__':
     fb_connection = firebirdsql.connect(
-        host='localhost',
-        database='/firebird/data/sample_database.fdb',
-        user='sysdba',
-        password='masterkey',
+        host=os.getenv('FIREBIRD_HOST', 'localhost'),
+        database=os.getenv('FIREBIRD_DATABASE', '/firebird/data/sample_database.fdb'),
+        user=os.getenv('FIREBIRD_USER', 'sysdba'),
+        password=os.getenv('FIREBIRD_PASSWORD', 'masterkey'),
         charset='WIN1252'
     )
 
     print('Connecting to PostgreSQL...')
     pg_connection = psycopg2.connect(
-        host='localhost',
-        dbname='sample_database',
-        user='postgres',
-        password='mypassword'
+        host=os.getenv('POSTGRES_HOST', 'localhost'),
+        port=int(os.getenv('POSTGRES_PORT', 5432)),
+        dbname=os.getenv('POSTGRES_DB', 'sample_database'),
+        user=os.getenv('POSTGRES_USER', 'postgres'),
+        password=os.getenv('POSTGRES_PASSWORD', 'mypassword')
     )
 
     migrator = DatabaseMigrator(fb_connection, pg_connection)
@@ -26,6 +31,7 @@ if __name__ == '__main__':
 
     migrator.import_data()
 
+    migrator.export_firebird_domains()
     migrator.export_firebird_triggers()
     migrator.export_firebird_procedures()
     migrator.export_firebird_views()
